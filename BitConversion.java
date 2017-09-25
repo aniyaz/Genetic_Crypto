86095ffb8de100f2fca9d773e15b28b05e1975ff
Aman <amanniyaz.waplog@gmail.com>
2017-09-25 08:03:55 +0530

    First commit

---
+++ BitConversion.java
@@ -0,0 +1,35 @@
+import java.io.*;
+class BitConversion
+{
+	
+	public void convert(int n, int bit[])
+	{
+		int mask;
+		mask = 128;
+		int i = 0, k;
+		while(mask != 0)
+		{
+			if((n & mask) == 0)
+				k = 0;
+			else
+				k = 1;
+			bit[i++] = k;
+			mask = mask >> 1;
+		}
+	}
+	public void toBits(String file1, String file2) throws IOException
+	{
+		int ch, i;
+		int bit[] = new int[8];
+		FileInputStream fp1 = new FileInputStream(file1);
+		FileOutputStream fp2 = new FileOutputStream(file2);
+		while((ch = fp1.read()) != -1)
+		{
+			convert(ch, bit);
+			for(i = 0; i < 8; i++)
+				fp2.write(bit[i] + 48);
+		}
+		fp1.close();
+		fp2.close();
+	}
+}
